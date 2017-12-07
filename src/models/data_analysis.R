8a89a2d6db7778178ef0ff98245cea1e68b081d2
estebanangelm <estebanangelm@gmail.com>
2017-12-06 22:34:43 -0800

    Include functionality for exporting figures to png

--- src/models/data_analysis.R
+++ src/models/data_analysis.R
@@ -17,6 +17,8 @@ hyp_1_plot_1 <- ggplot(var_price_revenue)+
   scale_x_continuous("Revenue Variation",labels = scales::percent_format())+
   ggtitle("Price Variation vs Revenue Variation")
 
+ggsave("../../results/figures/hyp_1_plot_1.png")
+
 (hyp_1_plot_2 <- ggplot(var_price_revenue %>% filter(var_revenue != "NA"))+
   geom_boxplot(aes(x=var_revenue>0,y=var_price),alpha=0.3,color="#4B86B4",fill="#4B86B4")+
   theme_minimal()+
@@ -24,9 +26,11 @@ hyp_1_plot_1 <- ggplot(var_price_revenue)+
   scale_x_discrete("YoY Revenue Increase")+
   ggtitle("Price Variation vs Revenue Variation"))
 
+ggsave("../../results/figures/hyp_1_plot_2.png")
+
 #Hypothesis 2
 
-(hyp_2_plot <- ggplot(var_price_margin)+
+(hyp_2_plot_1 <- ggplot(var_price_margin)+
   geom_point(aes(x=operating_margin,y=var_price),alpha=0.3,color="#4B86B4")+
   coord_cartesian(xlim = c(-100,100))+
   theme_minimal()+
@@ -35,18 +39,18 @@ hyp_1_plot_1 <- ggplot(var_price_revenue)+
   scale_x_continuous("Revenue Variation")+
   ggtitle("Price Variation vs Revenue Variation"))
 
-
+ggsave("../../results/figures/hyp_1_plot_1.png")
 
 #EDA 1
 
-prices_sector <- var_price_revenue %>% inner_join(securities,by = c("ticker" = "Ticker symbol")) %>% 
-  select(ticker,var_price,sector = `GICS Sector`)
-
-ggplot(prices_sector)+
+ggplot(price_sector)+
   geom_boxplot(aes(x=sector,y=var_price,fill=(sector == "Information Technology")),alpha=0.6)+
-  geom_jitter(aes(x=sector,y=var_price),alpha=0.6,color="#4B86B4")+
+  geom_jitter(aes(x=sector,y=var_price),alpha=0.3,color="#4B86B4")+
   theme_minimal()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1),legend.position="none")
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),legend.position="none")+
+  scale_y_continuous("Price Variation",labels = scales::percent_format())
+
+ggsave("../../results/figures/eda_plot_1.png")
 
 
 options(warn = 0)
