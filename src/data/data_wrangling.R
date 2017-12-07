5f11ece1f00fe4df2396ad991afb236e70476e53
estebanangelm <estebanangelm@gmail.com>
2017-12-06 22:01:07 -0800

    Add new data wrangling chunk for hypothesis 2

--- src/data/data_wrangling.R
+++ src/data/data_wrangling.R
@@ -9,18 +9,32 @@ securities <- read_csv('../../data/original/securities.csv')
 #This code gets the data from Fundamentals and creates two new columns with the variation year on year for revenue and price
 #Also, it discards the columns that are not important for this analysis
 
-var_price_revenue <- fundamentals %>% select(ticker = `Ticker Symbol`,
-                                             period = `Period Ending`,
-                                             total_revenue = `Total Revenue`) %>%
-  left_join(prices,by=c("ticker" = "symbol","period" = "date")) %>% 
-  select(ticker,period,total_revenue,price = close) %>% 
-  group_by(ticker) %>% 
-  mutate(var_revenue = total_revenue / lag(total_revenue) - 1,
-         var_price = price / lag(price) - 1)
+var_price_revenue <- fundamentals %>%  select(ticker = `Ticker Symbol`,
+                                              period = `Period Ending`,
+                                              total_revenue = `Total Revenue`) %>%
+                                        left_join(prices,by=c("ticker" = "symbol","period" = "date")) %>% 
+                                        select(ticker,period,total_revenue,price = close) %>% 
+                                        group_by(ticker) %>% 
+                                        mutate(var_revenue = total_revenue / lag(total_revenue) - 1,
+                                               var_price = price / lag(price) - 1)
 
 #This code saves the previous wrangled dataframe in a CSV in the data folder. Will help solve hypothesis 1.
 
 write_csv(var_price_revenue,'../../data/processed/var_price_revenue.csv')
+
+#This code compares the profitability of the companies and their year on year returns
+
+var_price_margin <- fundamentals %>%  select(ticker = `Ticker Symbol`,
+                                              period = `Period Ending`,
+                                              operating_margin = `Operating Margin`) %>%
+                                        left_join(prices,by=c("ticker" = "symbol","period" = "date")) %>% 
+                                        select(ticker,period,operating_margin,price = close) %>% 
+                                        group_by(ticker) %>% 
+                                        mutate(var_price = price / lag(price) - 1)
+
+#This code saves the previous wrangled dataframe in a CSV in the data folder. Will help solve hypothesis 1.
+
+write_csv(var_price_margin,'../../data/processed/var_price_margin.csv')
 
 #The dataframe for hypothesis 2 requires a join between the var_price_revenue dataset and the securities dataset
 
