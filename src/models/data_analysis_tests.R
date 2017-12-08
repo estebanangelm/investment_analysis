03e8a73e9045bd8340f13d1b33fc358b03a847bd
estebanangelm <estebanangelm@gmail.com>
2017-12-07 16:36:59 -0800

    Separate analysis in two files, one for figures and one for statistical tests

---
+++ src/models/data_analysis_tests.R
@@ -0,0 +1,30 @@
+# Esteban Angel, Dec 2017
+#
+# This script takes the processed datasets and creates some plots and statistical analysis for testing the hypotheses. 
+#
+# Usage: Rscript data_analysis.R data/processed/var_price_revenue.csv data/processed/var_price_margin.csv data/processed/price_sector.csv data/processed/market_cap_sector.csv results/figures/hyp_1_plot_1.png results/figures/hyp_1_plot_2.png results/figures/hyp_2_plot_1.png results/figures/eda_plot_1.png results/figures/eda_plot_2.png
+
+library(tidyverse)
+library(forcats)
+
+options(warn = -1)
+
+root <- "../../"
+
+args <- commandArgs(trailingOnly = TRUE)
+
+origin_1 <- args[1]
+origin_2 <- args[2]
+origin_3 <- args[3]
+origin_4 <- args[4]
+
+destination_1 <- args[5]
+destination_2 <- args[6]
+destination_3 <- args[7]
+destination_4 <- args[8]
+destination_5 <- args[9]
+
+var_price_revenue <- read_csv(paste(root,origin_1,sep=""))
+var_price_margin <- read_csv(paste(root,origin_2,sep=""))
+price_sector <- read_csv(paste(root,origin_3,sep=""))
+market_cap_sector <- read_csv(paste(root,origin_4,sep=""))
